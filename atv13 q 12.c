442b3520b49f172359e3f3579e91e75add14f9be
JuliaSilva05 <128699860+JuliaSilva05@users.noreply.github.com>
2023-10-16 18:23:53 -0300

    Update atv13 q 12.c

--- atv13 q 12.c
+++ atv13 q 12.c
@@ -9,12 +9,16 @@ int main()
     char letra;
     printf("Insira uma letra: \n");
     scanf("%c",&letra);
-    int i = 0;    
-    for (i = 0; i < 100; i++){
+    int i = 0;   
+    int vogais = 0; 
+    for (i = 0; str[i]!='\0'; i++){
         if (str[i]=='a'||str[i]=='A'||str[i]=='e'||str[i]=='E'||str[i]=='i'||str[i]=='I'||str[i]=='o'||str[i]=='O'||str[i]=='u'||str[i]=='U'){
             str[i]=letra;
+            vogais = vogais + 1;
         }
+        
     }
+    printf("Vogais: %d\n",vogais);
     printf("%s",str);
     return 0;
 }
